ba6897e639136d4bae473621675d98b87e5224c6
Abdurahmon <abduqodirovabdurakhmon@gmail.com>
2024-08-21 14:31:07 +0500

    site-name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,46 +6,42 @@ import Telegram from "../../public/telegram.svg";
 import Instagram from "../../public/instagram.svg";
 import Facebook from "../../public/facebook.svg";
 
-
-
 const Footer = () => {
   return (
-    <div className="bg-[#0e685a] h-[350px]">
+    <div className="bg-[#0e685a] md:h-[350px]">
       <div className="p-8">
-        <div className=" flex justify-between">
+        <div className="flex flex-col md:flex-row justify-between">
           <img
             src={Footer_logo}
             alt="logo"
-            className="w-[260px] mt-[-60px] ml-[30px]"
+            className="w-[260px] md:mt-[-60px] ml-[30px] mx-auto md:mx-0"
           />
-          <div>
-            <div className=" flex mt-[20px] mb-[40px]">
+          <div className="mt-4 md:mt-0">
+            <div className="flex flex-row md:flex-row items-start md:items-center mt-4 mb-[20px] md:mb-[40px]">
               <img
                 src={Location_image}
                 alt="image"
-                className=" mt-2 mr-[12px] h-[30px]"
+                className="mt-2 mr-[12px] h-[20px] md:h-[30px]"
               />
-              <h3 className=" text-xl font-normal text-white w-[700px]">
-                Узбекистан, Ташкент, Мизро-Улугбекский район, улица Чуст, дом
-                15. Ориентир: станция метро Буюк Ипак Йули; (бывш. Максим
-                Горький)
+              <h3 className="text-base md:text-xl font-normal text-white">
+                Узбекистан, Ташкент, Мизро-Улугбекский район
               </h3>
             </div>
-            <div className=" flex justify-around">
-              <div className=" flex">
+            <div className="flex flex-col md:flex-row md:justify-around">
+              <div className="flex items-center mb-2 md:mb-0">
                 <img src={Phone_image} alt="image" className="h-[20px]" />
                 <a
-                  className="text-white font-semibold text-xl ml-2 mt-[-6px]"
+                  className="text-white font-semibold text-base md:text-xl ml-2"
                   href="tel:+998712001133"
                 >
                   +998 71 200 11 33
                 </a>
               </div>
-              <div className=" flex">
+              <div className="flex items-center">
                 <img src={Mail} alt="image" className="h-[20px]" />
                 <a
                   href="mailto:info@ritm.uz"
-                  className="text-white font-semibold text-xl ml-2 mt-[-6px]"
+                  className="text-white font-semibold text-base md:text-xl ml-2"
                 >
                   info@ritm.uz
                 </a>
@@ -54,28 +50,30 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <hr className=" opacity-55 mt-[30px] w-screen" />
-      <div className=" flex mt-[20px]">
-        <h3 className=" ml-[40px] text-white text-[17px] opacity-80 mt-[40px]">
+      <hr className="opacity-55 mt-[30px] w-full" />
+      <div className="flex flex-col md:flex-row justify-between items-center mt-4">
+        <h3 className="text-white text-sm md:text-[17px] opacity-80">
           © 2024 - 2028 Copyright. HouseLab. Все права защищены.
         </h3>
-        <div className=" flex space-x-4 mt-[20px] ml-[700px]">
-         <button 
-         onClick={() => (window.location.href = " https://t.me/endless_live ")}
-         className="hover:scale-110 transition-transform  w-[40px] h-[40px] bg-[#28a18f] rounded-3xl">
-         <img src={Telegram} alt="icon" className="h-[30px] ml-1" />
-         </button>
-         <button
-         onClick={() => (window.location.href = " https://www.instagram.com/abdukadirovv._a/ ")}
-         className=" hover:scale-110 transition-transform  w-[40px] h-[40px] bg-[#28a18f] rounded-3xl">
-          <img src={Instagram} alt="icon" className="h-[22px] ml-[9px]" />
-         </button>
-         <button
-         onClick={() => (window.location.href = "https://www.facebook.com/profile.php?id=100089769144656")}
-         className=" hover:scale-110 transition-transform  w-[40px] h-[40px]  bg-[#28a18f] rounded-3xl">
-          <img src={Facebook} alt="icon" className="h-[25px] ml-2" />
-         </button>
-
+        <div className="flex space-x-4 mt-2 md:mt-0 mb-[20px]">
+          <button 
+            onClick={() => (window.location.href = "https://t.me/endless_live")}
+            className="hover:scale-110 transition-transform w-[40px] h-[40px] bg-[#28a18f] rounded-full flex items-center justify-center"
+          >
+            <img src={Telegram} alt="icon" className="h-[30px]" />
+          </button>
+          <button
+            onClick={() => (window.location.href = "https://www.instagram.com/abdukadirovv._a/")}
+            className="hover:scale-110 transition-transform w-[40px] h-[40px] bg-[#28a18f] rounded-full flex items-center justify-center"
+          >
+            <img src={Instagram} alt="icon" className="h-[22px]" />
+          </button>
+          <button
+            onClick={() => (window.location.href = "https://www.facebook.com/profile.php?id=100089769144656")}
+            className="hover:scale-110 transition-transform w-[40px] h-[40px] bg-[#28a18f] rounded-full flex items-center justify-center"
+          >
+            <img src={Facebook} alt="icon" className="h-[25px]" />
+          </button>
         </div>
       </div>
     </div>
